3c90555cbcbaaf0e7b291839849655deab6231b0
Brian  Crowley <briancrowley@Brians-MacBook-Pro.local>
2018-02-20 11:41:22 +0000

    message images

---
+++ howdy-chat-app/View/CircleImageView.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImageView.swift
+//  howdy-chat-app
+//
+//  Created by Brian  Crowley on 20/02/2018.
+//  Copyright © 2018 Brian Crowley. All rights reserved.
+//
+
+import UIKit
+
+class CircleImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
